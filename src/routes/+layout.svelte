de0f67c578fdb7ca6cdac1cf39c70e4c08cc2a99
mnyrop <marii@nyu.edu>
2024-12-04 18:07:44 -0500

    init ui w/ carbon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,40 @@
 <script>
 	import '../app.css';
+	import { ExpandableTile } from "carbon-components-svelte";
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="min-h-screen p-12">
+	<header>
+		<h1 class="mb-6">M/S Sample Document Explorer</h1>
+		<div class="flex md:flex-nowrap flex-wrap w-full gap-10">
+			<div class="md:basis-1/2">
+				<ExpandableTile light class="max-w-prose" tileExpandedLabel="Read less" tileCollapsedLabel="Read more">
+					<div slot="above">
+						<h2 class="py-6">About</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet malesuada odio. Sed laoreet tortor eu orci ornare, condimentum commodo metus egestas. Etiam in velit molestie, volutpat turpis id, faucibus odio. Vivamus gravida nunc et tincidunt vehicula. Nulla nec convallis eros. Aenean a porttitor metus. Praesent convallis sem non lacus lacinia faucibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; </p>
+					</div>
+					<div slot="below" class="py-6">
+						<p>Proin consectetur enim ornare dolor mollis, tincidunt tincidunt erat pellentesque. Quisque varius, nunc vel condimentum dignissim, turpis magna consectetur magna, vitae posuere ligula nibh nec nisl. Suspendisse imperdiet elit eros, quis scelerisque ante pharetra non. Proin sodales convallis lacus vitae vestibulum. Donec vitae felis purus. Nulla ullamcorper venenatis varius. Vivamus varius in diam ut iaculis. Curabitur elementum risus non accumsan tincidunt. </p>
+					</div>
+				</ExpandableTile>
+			</div>
+			<div class="md:basis-1/2">
+				<ExpandableTile light class="max-w-prose" tileExpandedLabel="Read less" tileCollapsedLabel="Read more">
+					<div slot="above">
+						<h2 class="py-6">Statistics</h2>
+						<p>A files in sample: 0</p>
+						<p>Pages in sample: 0</p>
+					</div>
+					<div slot="below" class="py-6">
+						<p>A files processed: 0</p>
+						<p>Pages processed: 0</p>
+					</div>
+				</ExpandableTile>
+			</div>
+		</div>
+	</header>
+	<main class="w-full">
+		{@render children()}
+	</main>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,200 @@
 <script>
   import { onMount } from 'svelte';
   import { addDocument, search } from '$lib/search';
+  
+  import {
+    Form, FormGroup,
+    NumberInput,
+    Select, SelectItem,
+    RadioButtonGroup, RadioButton,
+    MultiSelect,
+    TextInput,
+    Button,
+    Tile,
+    Tag,
+    Tabs, Tab, TabContent
+  } from "carbon-components-svelte";
+
 
   let query = '';
   let results = [];
+  let selectedFields = [];
+  let yearMin = 1800;
+  let yearMax = 1980;
 
   // Sample documents
   const documents = [
-      { id: 1, content: 'First document content' },
-      { id: 2, content: 'Second document content' },
+    { id: 1, content: 'First document content' },
+    { id: 2, content: 'Second document content' },
   ];
 
   onMount(() => {
-      documents.forEach(doc => addDocument(doc.id, doc.content));
+    documents.forEach(doc => addDocument(doc.id, doc.content));
   });
 
   function handleSearch() {
-      results = search(query);
+    results = search(query, {suggest: true});
   }
+  
+  $: console.log('query length', query.length);
+  $: isSearchInvalid = selectedFields.length === 0 && query.length > 0;
+  $: selectedFieldsLabel = selectedFields.length === 0 ? 'Select' : selectedFields.join(', ');
 </script>
 
-<input bind:value={query} on:input={handleSearch} placeholder="Search..." />
-<ul>
-  {#each results as result}
-      <li>{result}</li>
-  {/each}
-</ul>
+
+<Tabs type="container" class="py-12">
+  <Tab label="Find A-Files" />
+  <Tab label="Find Pages" />
+  <svelte:fragment slot="content">
+    <Tile>
+    <TabContent>
+      
+        <Form on:submit={(e) => {
+          e.preventDefault();
+          handleSearch();
+        }}>
+          <div class="py-4 font-bold">Search Within</div>
+          <FormGroup legendText="Fields">
+            <div class="flex flex-row justify-start">
+              <div class="basis-1/3">
+                <MultiSelect
+                  label={selectedFieldsLabel}
+                  selectionFeedback="fixed"
+                  bind:selectedIds={selectedFields}
+                  size="lg"
+                  useTitleInItem=true
+                  items={[
+                    { id: "A-Number", text: "A-Number (NARA)" },
+                    { id: "CoB", text: "Country of birth (NARA)" },
+                    { id: "Last Name", text: "Last name (NARA)" },
+                    { id: "First Name", text: "First name (NARA)" },
+                    { id: "PoE", text: "Port of entry (NARA)"}
+                  ]}
+                />
+              </div>
+              <div class="basis-2/3">
+                <TextInput placeholder="Search..." invalidText="Select 1+ fields for the search" bind:value={query} invalid={isSearchInvalid}/>
+              </div>
+            </div>
+          </FormGroup>
+
+          <div class="py-4 font-bold">Filter by Date Range</div>
+          <FormGroup>     
+            <div class="py-2">Date of Entry (NARA)</div>
+            <div class="flex justify-start py-2">
+              <div class="basis-1/2">
+                <NumberInput inline label="Start Year" value={yearMin} min={yearMin} max={yearMax} invalidText="Number must be between {yearMin} and {yearMax}."/>
+              </div>
+              <div class="basis-1/2">
+                <NumberInput inline label="End Year" value={yearMax} min={yearMin} max={yearMax} invalidText="Number must be between {yearMin} and {yearMax}."/>
+              </div>
+            </div>
+
+            <div class="py-2">Date of Birth (NARA)</div>
+            <div class="flex justify-start py-2">
+              <div class="basis-1/2">
+                <NumberInput inline label="Start Year" value={yearMin} min={yearMin} max={yearMax} invalidText="Number must be between {yearMin} and {yearMax}."/>
+              </div>
+              <div class="basis-1/2">
+                <NumberInput inline label="End Year" value={yearMax} min={yearMin} max={yearMax} invalidText="Number must be between {yearMin} and {yearMax}."/>
+              </div>
+            </div>
+          </FormGroup> 
+
+          <div class="py-4 font-bold">Segment</div>
+          <FormGroup>
+            <RadioButtonGroup legendText="Sex (NARA)" name="sex" selected="any">
+              <RadioButton labelText="Any" value="any" />
+              <RadioButton labelText="Unset" value="unset" />
+              <RadioButton labelText="F" value="F" />
+              <RadioButton labelText="M" value="M" />
+            </RadioButtonGroup>
+          </FormGroup>
+
+          
+
+          <Button on:click={handleSearch}>Submit</Button>
+        </Form>
+
+        <div>
+          <ul>
+            {#each results as result}
+              <li>{result}</li>
+            {/each}
+          </ul>
+        </div>
+
+    </TabContent>
+
+
+    <TabContent>
+      <form>
+        <label>
+          <div>
+            <span>Search within a field</span>
+          </div>
+          <div>
+            <select size="1" aria-label="select field to search within" id="search-limit-select">
+              <option>All</option>
+              <option>OCR Text</option>
+              <option>Countries (NLP)</option>
+              <option>Form Title (LLM)</option>
+            </select>
+            <input size="1" id="search-input" bind:value={query} on:input={handleSearch} placeholder="Search"/>
+            <button size="1" aria-label="submit search query" id="search-submit">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"><path fill-rule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clip-rule="evenodd" /></svg>
+            </button>
+          </div>
+        </label>
+      </form>
+  
+      <form>
+        <label>
+          <div>
+            <span>Search for Date of Birth (LLM) within a range</span>
+          </div>
+          <div>
+            <input size="1" id="search-range-start" placeholder="Start"/>
+            <input size="1" id="search-range-end" placeholder="End"/>
+            <button size="1" aria-label="submit search range" id="search-range-submit">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"><path fill-rule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clip-rule="evenodd" /></svg>
+            </button>
+          </div>
+        </label>
+      </form>
+  
+      <form>
+        <label>
+          <div>
+            <span>Facet by document type (CNN)</span>
+          </div>
+          <div>
+            <select size="1" aria-label="select document type to facet" id="facet-select">
+              <option>Form</option>
+              <option>Letter</option>
+              <option>Photograph</option>
+              <option>Miscellaneous</option>
+            </select>
+          </div>
+        </label>
+      </form>
+  
+      <br>
+      <form>
+        <label>
+          <div>
+            <span>Facet by sex (LLM)</span>
+          </div>
+          <div>
+            <select size="1" aria-label="select document type to facet" id="facet-select">
+              <option>Female</option>
+              <option>Male</option>
+            </select>
+          </div>
+        </label>
+      </form>
+
+    </TabContent>
+    </Tile>
+  </svelte:fragment>
+</Tabs>
