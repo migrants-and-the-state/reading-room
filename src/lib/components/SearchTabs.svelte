80972f91c5ebfc6914d3d6245163f5888a3d8027
mnyrop <marii@nyu.edu>
2024-12-12 16:30:40 -0500

    init reactive scope tabbing

--- src/lib/components/SearchTabs.svelte
+++ src/lib/components/SearchTabs.svelte
@@ -1,18 +1,32 @@
 <script>
 	import { base } from '$app/paths';
+	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
+
 	import AFileSearch from '$lib/components/search/AFile.svelte';
 	import PageSearch from '$lib/components/search/Page.svelte';
 	import G325ASearch from '$lib/components/search/G325A.svelte';
 	import NatCertSearch from '$lib/components/search/NatCert.svelte';
 
 	import { Tabs, Tab, TabContent } from 'carbon-components-svelte';
+
+	const validScopes = ['afile', 'page', 'g325a', 'natcert'];
+
+	$: scope = ($page.url.searchParams.get('scope') || '').toLowerCase();
+	$: scopeIndex = validScopes.indexOf(scope) !== -1 ? validScopes.indexOf(scope) : 0;
+
+	function updateScope(index) {
+		const newScope = validScopes[index];
+		$page.url.searchParams.set('scope', newScope);
+		goto(`?${$page.url.searchParams.toString()}`);
+	}
 </script>
 
-<Tabs type="container" autoWidth>
-	<Tab label="Find A-Files" />
-	<Tab label="Find Pages" />
-	<Tab label="Find G325A Forms *" />
-	<Tab label="Find Naturalization Certificates *" />
+<Tabs type="container" selected={scopeIndex} autoWidth on:change={(e) => updateScope(e.detail)}>
+	<Tab id="afile" label="Find A-Files" />
+	<Tab id="page" label="Find Pages" />
+	<Tab id="g325a" label="Find G325A Forms *" />
+	<Tab id="natcert" label="Find Naturalization Certificates *" />
 	<svelte:fragment slot="content">
 		<TabContent class="bg-white">
 			<AFileSearch />
