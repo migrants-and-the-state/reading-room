f0bd3b3fe70261fce3ecfa8f4f79036ba7b1d42a
mnyrop <marii@nyu.edu>
2025-02-06 10:47:06 -0500

    link stats, unlink terms

--- src/lib/components/InfoPane.svelte
+++ src/lib/components/InfoPane.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { base } from '$app/paths';
+	import { goto } from '$app/navigation';
 	import { updateScopeIndex } from '$lib/scope';
 	import {
 		StructuredList,
@@ -14,20 +15,35 @@
 		Identification as G325AIcon,
 		Policy as NatCertIcon
 	} from 'carbon-icons-svelte';
+
+	const stats = {
+		afile: { label: 'A-Files', sample: 423, full: 751 },
+		page: { label: 'Pages', sample: 20516, full: 37396 },
+		g325a: { label: 'G325 Forms *', sample: 504, full: 'unknown' },
+		natcert: { label: 'Naturalization Certificates *', sample: 87, full: 'unknown' }
+	};
 </script>
 
-<div class="flex w-full flex-wrap md:flex-nowrap md:gap-10">
-	<div class="basis-full md:basis-1/2 my-6">
+<div class="mb-6 flex w-full flex-wrap md:flex-nowrap md:gap-10">
+	<div class="my-6 basis-full md:basis-1/2">
 		<p class="py-2">
-			<a href="https://migrants-and-the-state.github.io/" target="_blank">Migrants and the State (M/S)</a>
-			aims to provide large-scale access to the public domain migrant records (A-Files) held by the <a href="https://www.archives.gov/" target="_blank">U.S.
-			National Archives (NARA)</a>. This prototype is designed to gather feedback on our current methods of enhancing A-file searchability and 
-			solicit ideas for future project development. </p>
+			<a href="https://migrants-and-the-state.github.io/" target="_blank"
+				>Migrants and the State (M/S)</a
+			>
+			aims to provide large-scale access to the public domain migrant records (A-Files) held by the
+			<a href="https://www.archives.gov/" target="_blank">U.S. National Archives (NARA)</a>. This
+			prototype is designed to gather feedback on our current methods of enhancing A-file
+			searchability and solicit ideas for future project development.
+		</p>
 		<p class="py-2">
-			Below you can search for entire <a href="?tab=afile">A-Files</a> or specific <a href="?tab=page">Pages</a> within them using both NARA-cataloged 
-			and experimentally-extracted metadata. Some pages have been machine-identified as <a href="">G-325 Forms</a> or <a href="">Naturalization Certificates</a>; these special pages include additional metadata fields created by our project team.
-			For more detailed information on our data curation choices and machine
-			learning model training, please see the <a href="{base}/data-guide">Data Guide</a>.
+			Below you can search for entire <span class="font-semibold">A-Files</span> or specific
+			<span class="font-semibold">Pages</span>
+			within them using both NARA-cataloged and experimentally-extracted metadata. Some pages have been
+			machine-identified as <span class="font-semibold">G-325 Forms</span> or
+			<span class="font-semibold">Naturalization Certificates</span>; these special pages include
+			additional metadata fields created by our project team. For more detailed information on our
+			data curation choices and machine learning model training, please see the
+			<a href="{base}/data-guide">Data Guide</a>.
 		</p>
 	</div>
 	<div class="basis-full md:basis-1/2">
@@ -41,32 +57,15 @@
 					</StructuredListRow>
 				</StructuredListHead>
 				<StructuredListBody>
-					<StructuredListRow>
-						<StructuredListCell noWrap><AFileIcon class="inline" /> A-Files</StructuredListCell>
-						<StructuredListCell>423</StructuredListCell>
-						<StructuredListCell>751</StructuredListCell>
-					</StructuredListRow>
-
-					<StructuredListRow>
-						<StructuredListCell noWrap><PageIcon class="inline" /> Pages</StructuredListCell>
-						<StructuredListCell>20,516</StructuredListCell>
-						<StructuredListCell>37,396</StructuredListCell>
-					</StructuredListRow>
-
-					<StructuredListRow>
-						<StructuredListCell noWrap><G325AIcon class="inline" /> G325 Forms *</StructuredListCell
-						>
-						<StructuredListCell>504</StructuredListCell>
-						<StructuredListCell>?</StructuredListCell>
-					</StructuredListRow>
-
-					<StructuredListRow>
-						<StructuredListCell noWrap
-							><NatCertIcon class="inline" /> Naturalization Certificates *</StructuredListCell
-						>
-						<StructuredListCell>87</StructuredListCell>
-						<StructuredListCell>?</StructuredListCell>
-					</StructuredListRow>
+					{#each Object.keys(stats) as key}
+						<StructuredListRow>
+							<StructuredListCell noWrap>{stats[key].label || key}</StructuredListCell>
+							<StructuredListCell>
+								<a href="{base}/results/{key}?query=">{stats[key].sample.toLocaleString()}</a>
+							</StructuredListCell>
+							<StructuredListCell>{stats[key].full.toLocaleString()}</StructuredListCell>
+						</StructuredListRow>
+					{/each}
 				</StructuredListBody>
 			</StructuredList>
 			<div class="py-1 pl-5 text-xs opacity-70">* As identified by M/S heuristic analysis</div>
